5f8438db5126572d6a655faa56e213fdee9a9629
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2022-05-05 01:43:07 +0700

    structure project

---
+++ src/components/ProductMix.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { productName } = defineProps<{
+  productName: string
+}>()
+</script>
+
+<template>
+  <div class="product-mix">
+    <h3>{{ productName }}</h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/BoardView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ProductMix from "@/components/ProductMix.vue"
+</script>
+
+<template>
+  <main>
+    <ProductMix product-name="abc"/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
