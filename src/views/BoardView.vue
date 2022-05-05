d569ee879fb8fee8a2d7a37cbdef6d1fd2fd189f
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2022-05-06 01:49:21 +0700

    start dragndrop feature

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,15 +1,34 @@
-<script setup lang="ts">
-import ProductMix from "@/components/ProductMix.vue"
-</script>
-
 <template>
-  <div class="side-bar">
-
-  </div>
   <main>
+    <div 
+      class="board-canvas" 
+      @dragenter.prevent="enterZone"
+      @dragleave.prevent="leaveZone"
+      @dragover.prevent="moveAround"
+      @drop.prevent="savePosition"
+    ></div>
   </main>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+function enterZone(e: DragEvent) {}
+function leaveZone(e: DragEvent) {}
+function moveAround(e: DragEvent) {}
+function savePosition(e: DragEvent) {}
+</script>
 
+<style scoped>
+main {
+  padding: 0;
+  width: 100%;
+  height: calc(100vh - var(--header-size));
+  border-right: 1px solid var(--color-border);
+  border-bottom: 1px solid var(--color-border);
+  overflow: auto;
+}
+.board-canvas {
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-board-background);
+}
 </style>
