f3e346aa924bdbe73bc9f5f3c31d2fd84c958a84
dev.thanapon@gmail.com <dev.thanapon@gmail.com>
2022-05-11 16:37:44 +0700

    project tree

---
+++ src/components/header/ProjectTree.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="project-tree">
+    <ul
+      class="structure-tree-list"
+    >
+      <li
+        class="tree-item"
+        v-for="(t, index) in tree"
+        :key="index"
+      >{{t}}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const {tree} = defineProps<{tree: string[]}>()
+
+</script>
+
+<style scoped>
+  
+</style>
