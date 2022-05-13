1960ef0d55532cf259bced29fe45a9dc9385a5b1
dev.thanapon@gmail.com <dev.thanapon@gmail.com>
2022-05-13 19:09:06 +0700

    move wrapper components

--- src/components/DragBoard/dragablePin/DragItem.vue
+++ src/components/DragBoard/dragablePin/DragItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="drag-item" draggable="true">
 
   </div>
 </template>
--- src/components/DragBoard/dragabletTools/DragTool.vue
+++ src/components/DragBoard/dragabletTools/DragTool.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="drag-tools" draggable="true">
 
   </div>
 </template>
