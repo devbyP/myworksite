e1f8be3f2e36159962230adb7feb4e6df3c3f663
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2022-05-19 00:35:01 +0700

    make grid view

--- src/components/setBoard/SetBoard.vue
+++ src/components/setBoard/SetBoard.vue
@@ -7,35 +7,39 @@
       @open-all="toggleAll(true)"
       class="show-pos"
     />
-    <div class="project-sec">
+    <div class="section project-sec">
       <StatusTab
         :title="projectSection.name"
         :is-open="projectSection.enable"
         @toggle="toggleWindow(projectSection.name, 0)"
+        class="tab"
       />
       <ProjectWindow v-show="projectSection.enable"/>
     </div>
-    <div class="budget-sec">
+    <div class="section budget-sec">
       <StatusTab
         :title="budgetSection.name"
         :is-open="budgetSection.enable"
         @toggle="toggleWindow(budgetSection.name, 1)"
+        class="tab"
       />
       <BudgetWindow v-show="budgetSection.enable" />
     </div>
-    <div class="ideas-sec">
+    <div class="section ideas-sec">
       <StatusTab
         :title="ideasSection.name"
         :is-open="ideasSection.enable"
         @toggle="toggleWindow(ideasSection.name, 2)"
+        class="tab"
       />
       <IdeaWindow v-show="ideasSection.enable"/>
     </div>
-    <div class="voting-sec">
+    <div class="section voting-sec">
       <StatusTab
         :title="votingSection.name"
         :is-open="votingSection.enable"
         @toggle="toggleWindow(votingSection.name, 3)"
+        class="tab"
       />
       <VotingWindow v-show="votingSection.enable"/>
     </div>
@@ -93,7 +97,18 @@ function toggleAll(open: boolean) {
 
 <style lang="scss" scoped>
 .set-board {
+  width: calc(100% - 80px);
+  height: $board-height;
   display:grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  .section {
+    border-style: solid;
+    border-width: 1px 1px 0 0;
+    border-color: $color-border;
+    max-height: calc($board-height / 2);
+    overflow: auto;
+  }
 }
 .show-pos {
   position: absolute;
